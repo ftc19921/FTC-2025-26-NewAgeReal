2b94738ee7d1b86e31d471dd9a3aea20e973d784
morpheus221 <mxwalker44@gmail.com>
2026-01-06 17:06:21 -0500

    Emergency Revert

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/AutoScore.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/AutoScore.java
@@ -1,11 +1,12 @@
 package org.firstinspires.ftc.teamcode.Autos;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.Robot;
-
-public class AutoScore  {
-    /*
+@Autonomous
+public class AutoScore1_6 extends OpMode {
+    boolean end;
     double percentScreenOfTarget;
     //(In inches)
     double distanceToTarget;
@@ -26,21 +27,55 @@ public class AutoScore  {
     public void loop() {
         percentScreenOfTarget = robot.limeLight.LookTa();
         distanceToTarget = getDistanceFromPercent(percentScreenOfTarget);
-    }\
+        telemetry.addData("Distance to target: ",distanceToTarget);
+        telemetry.update();
+        //fireThisMany(2,0.5);
+        //fireThisMany(2,1);
+    }
+
+    public void MoveDistanceAwayFromTarget(double Power,double Distance /*in Inches*/){
+        while(Distance>distanceToTarget) {
+            if (robot.limeLight.detectID() == -1) {
+                end = true;
+                break;
+            } else {
+                robot.tankDrive.setDrivePowers(Power,Power);
+            }
+        }
+        while(Distance<distanceToTarget){
+            if (robot.limeLight.detectID() == -1) {
+                end = true;
+                break;
+            } else {
+                robot.tankDrive.setDrivePowers(-Power,-Power);
+            }
+        }
+    }
+    public void fireThisMany(double ArtifactsToFire, double power){
+        while(ArtifactsToFire>0){
+            robot.shooters.spinUpShooter(power);
+            waitToSpin(100000);
+            robot.shooters.fire();
+            waitToSpin(100000);
+            robot.shooters.spinUpShooter(0);
+            ArtifactsToFire--;
+        }
+    }
     public double getDistanceFromPercent(double Percent){
         //screenArea = distanceToTarget*(distanceToTarget*0.75);
         //targetArea = 36;//Inches squared
         //percentScreenOfTarget = (targetArea/screenArea)*100;
-
         //percentScreenOfTarget/100 = (targetArea/screenArea)
         //(100*targetArea)/percentScreenOfTarget = screenArea
         //(3600)/percentScreenOfTarget = distanceToTarget*(distanceToTarget*0.75);
-        //3600/percentScreenOfTarget =
-
-
-        return(Percent);
+        //3600/percentScreenOfTarget = distanceToTarget^2
+        return(Math.sqrt(3600/(percentScreenOfTarget*0.75)));
     }
-}
 
- */
+    public void waitToSpin(double Time){
+
+        while(Time>0){
+            Time--;
+        }
+    }
 }
